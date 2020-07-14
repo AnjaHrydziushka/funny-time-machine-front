d0de79da8cd986a9743b20f4a4fb557116a5e630
AnjaHrydziushka <anja.hryd@gmail.com>
2020-07-14 14:57:00 +0200

    Input form for QuizPage

--- src/pages/QuizPage.js
+++ src/pages/QuizPage.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 
 export default function QuizPage() {
     return (
@@ -6,8 +6,8 @@ export default function QuizPage() {
       <h1>Funny Time Machine</h1>
 
       <h2>What do you know about this place?</h2>
-      
-      
+      <p>What did Napoleon do with his brother Louis?</p>
+      <input type="text"></input>
       <button style={{ marginRight: "30px" }}> Submit </button>
     </div>
     )
