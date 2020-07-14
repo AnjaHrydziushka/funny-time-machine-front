2e907683400a69a8ac7eb6ec5aba9f44ae594a7e
AnjaHrydziushka <anja.hryd@gmail.com>
2020-07-14 14:56:42 +0200

    Added Router and Switch

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from "react";
 import "./App.css";
 import HomePage from "./pages/homepage";
+import QuizPage from './pages/QuizPage';
+import { Switch, Route } from "react-router-dom";
 
 function App() {
-  return <HomePage />;
+  return (
+    <div className="App">
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/quiz" component={QuizPage} />
+      </Switch>
+    </div>
+  )
 }
 
 export default App;
