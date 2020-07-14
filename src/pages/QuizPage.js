413a2533dad59dbb3547d6549fa2b84d0d67f4cc
AnjaHrydziushka <anja.hryd@gmail.com>
2020-07-14 14:50:58 +0200

    Created a page for the quiz

---
+++ src/pages/QuizPage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function QuizPage() {
+    return (
+        <div style={{ textAlign: "center" }}>
+      <h1>Funny Time Machine</h1>
+
+      <h2>What do you know about this place?</h2>
+      
+      
+      <button style={{ marginRight: "30px" }}> Submit </button>
+    </div>
+    )
+}
