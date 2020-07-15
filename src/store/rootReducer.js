3b6d8479ed775ecd9e5553b16e3c8bdb71827066
corina <corinabe93@gmail.com>
2020-07-15 13:35:33 +0200

    quizzes and answers added

--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -1,5 +1,8 @@
 import { combineReducers } from "redux";
+import quizzes from "./quizzes/reducer";
+import answers from "./answers/reducer";
 
 export default combineReducers({
- 
+  quizzes,
+  answers,
 });
