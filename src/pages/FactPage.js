4ac8629635f6a1d8825a1b4ba8f2de0294a02311
AnjaHrydziushka <anja.hryd@gmail.com>
2020-07-15 11:15:02 +0200

    Displayed the fact and its photo on a page

--- src/pages/FactPage.js
+++ src/pages/FactPage.js
@@ -13,19 +13,23 @@ export default function FactPage() {
     console.log("Selector:", factById)
 
     useEffect(() => {
-        dispatch(fetchFactsById(id))
+        if (factById.length === 0) {
+            dispatch(fetchFactsById(id))
+        }
     }, [])
+
+    if (!factById) return <div>Loading...</div>
 
     return (
         <div style={{ textAlign: "center" }}>
             <h1>Fun Facts</h1>
             <img 
-                src="https://upload.wikimedia.org/wikipedia/commons/1/1b/307-Portret_van_Lodewijk_Napoleon%2C_koning_van_Holland.jpg"
+                src={factById.imageUrl}
                 width="300px"
                 alt="Louis Bonaparte"
             />
             <h3>Did you know that...</h3>
-            <p>Napoleon made his brother Louis the king of Holland, but in a few years get a serious quarrel with his brother. Napoleon wanted Dutch troops for his invasion of Russia. Louis refused; Napoleon then suggested that Louis should abdicate but he refused. Napoleon removed Louis from the Dutch throne and took over the entire Kingdom of Holland on July 1, 1810.</p>
+            <p>{factById.content}</p>
             <h4>Your answer was:</h4>
             <p>Bonaparte sounds like some famouse footbal player...</p>
         </div>
