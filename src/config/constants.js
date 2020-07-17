b936b591eafd4a02d25e2706cf42433b15e46c9c
AnjaHrydziushka <anja.hryd@gmail.com>
2020-07-17 10:57:07 +0200

    Deploy to Netlify

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,3 +1,3 @@
-export const API_URL = process.env.API_URL || "http://localhost:4000";
+export const API_URL = process.env.API_URL || "http://localhost:4000" || "https://funnytimemachine.herokuapp.com/";
 export const DEFAULT_MESSAGE_TIMEOUT = 3000;
 
